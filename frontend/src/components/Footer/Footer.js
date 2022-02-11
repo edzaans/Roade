d96924150aba3d0144fe4a7fae017805e0d3a281
Edgars <edzaans@hotmail.com>
2022-02-11 17:49:19 +0000

    Updated Footer and Footer styles

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
 // Import Icon name to be used ( use as a component in code )
+import { Form, FormControl } from "react-bootstrap";
 import { AiFillFacebook } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
+import { FaYoutubeSquare } from "react-icons/fa";
 
 import { FaTwitterSquare } from "react-icons/fa";
+
+import { Button, Row, Col } from "react-bootstrap";
 
 // Import stylesheet
 import("./Footer.css");
@@ -11,58 +15,10 @@ function Footer() {
   return (
     <div className="text-center">
       <hr></hr>
-      {/*  */}
-      {/*       <Container className="footer">
-        <Row>
-          <Column sm={12} md={4}>
-            <Heading>About Us</Heading>
-            <FooterLink href="#">Aim</FooterLink>
-            <FooterLink href="#">Vision</FooterLink>
-            <FooterLink href="#">Testimonials</FooterLink>
-          </Column>
-
-          <Column sm={12} md={4}>
-            <Heading>Contact Us</Heading>
-            <FooterLink href="#">roade@support.ie</FooterLink>
-            <FooterLink href="#">+353 9100000</FooterLink>
-            <FooterLink href="#">www.roade.ie</FooterLink>
-          </Column>
-
-          <Column sm={12} md={4}>
-            <Heading>Social Media</Heading>
-            <Row>
-              <Column>
-                {" "}
-                <FooterLink href="#">
-                  <span>
-                    <AiFillFacebook className="icon icon_fb" />
-                  </span>
-                </FooterLink>
-              </Column>
-              <Column>
-                {" "}
-                <FooterLink href="#">
-                  <span>
-                    <AiFillInstagram className="icon icon_insta" />
-                  </span>
-                </FooterLink>
-              </Column>
-              <Column>
-                {" "}
-                <FooterLink href="#">
-                  <span>
-                    <FaTwitterSquare className="icon icon_twitter" />
-                  </span>
-                </FooterLink>
-              </Column>
-            </Row>
-          </Column>
-        </Row>
-      </Container> */}
 
       <div class=" container-fluid">
         <footer class="text-center text-white footer">
-          <section class="mt-5">
+          <section class="mt-3">
             <div class="row text-center d-flex justify-content-center pt-5">
               <div class="col-md-2">
                 <h6 class="text-uppercase font-weight-bold">
@@ -100,35 +56,47 @@ function Footer() {
 
           <hr class="my-5" />
 
-          <section className="mb-3">
-            <div class="row d-flex justify-content-center">
-              <div class="col-lg-8">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-                  distinctio earum repellat quaerat voluptatibus placeat nam,
-                  commodi optio pariatur est quia magnam eum harum corrupti
-                  dicta, aliquam sequi voluptate quas.
-                </p>
-              </div>
-            </div>
-          </section>
+          <Row className="text-center subscribe_container">
+            <Col sm={12} md={6}>
+              <section className="text-center">
+                <span>
+                  <h3 className="">Connect with Us</h3>
+                </span>
+                <span>
+                  <AiFillFacebook className="icon icon_fb" />
+                </span>
+                <span>
+                  <AiFillInstagram className="icon icon_insta" />
+                </span>
+                <span>
+                  <FaTwitterSquare className="icon icon_twitter" />
+                </span>
+                <span>
+                  <FaYoutubeSquare className="icon icon_yt" />
+                </span>
+              </section>
+            </Col>
+            <Col sm={12} md={6}>
+              <section className="">
+                <span>
+                  <h3 className="text-white mb-5">Subscribe to newsletter</h3>
+                </span>
+                <Form className="d-flex justify-content-center my-3">
+                  <FormControl
+                    type="email"
+                    placeholder="Your e-mail"
+                    className="me-2 subscribe"
+                    aria-label="subscribe"
+                  />
+                  <Button type="submit" className="">
+                    Subscribe
+                  </Button>
+                </Form>
+              </section>
+            </Col>
+          </Row>
 
-          <section class="text-center mb-5">
-            <span>
-              <h3 className="mb-5 links">Connect with US</h3>
-            </span>
-            <span>
-              <AiFillFacebook className="icon icon_fb" />
-            </span>
-            <span>
-              <AiFillInstagram className="icon icon_insta" />
-            </span>
-            <span>
-              <FaTwitterSquare className="icon icon_twitter" />
-            </span>
-          </section>
-
-          <div className="copyright pb-5">Copyright &copy; ROADE 2022</div>
+          <div className="copyright py-5">Copyright &copy; ROADE 2022</div>
         </footer>
       </div>
     </div>
